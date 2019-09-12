4c4823922147e8ccd51dc44ab74b6741cfdc4e35
Arkadiy Deliev <8557422+arkadiyjs@users.noreply.github.com>
2019-09-12 12:23:34 +0200

    Backup of BratMUN team page

---
+++ team.php
@@ -0,0 +1,296 @@
+﻿
+<!DOCTYPE html>
+
+<html lang="sk">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bratislava Model United Nations (BratMUN) is a high school United Nations simulation taking place annually in Bratislava. The conference has been organized since the year 2000 and has throughout its history attracted students from more than 20 countries.">
+
+    <!-- e.g. Baidu make use of meta keywords -->
+    <meta name="keywords" content="BratMUN | Home">
+
+    <meta name="author" content="Roman Beňo">
+    <title>BratMUN 2018 | Team</title>
+
+    <meta property="og:title" content="BratMUN">
+    <meta property="og:image" content="img/logo.png">
+    <meta property="og:description" content="Bratislava Model United Nations (BratMUN) is a high school United Nations simulation taking place annually in Bratislava. The conference has been organized since the year 2000 and has throughout its history attracted students from more than 20 countries.">
+
+    <?php require_once("parts/head-externals.php"); ?>
+
+    <!-- Team specific CSS -->
+    <link href="css/team.css" rel="stylesheet">
+
+    <script>
+        $(function()
+        {
+            makeNavbarItemActive({"#bj-main-navbar":"team"});
+            smoothScroll();
+        });
+    </script>
+
+</head>
+
+<body>
+
+<div id="main-content">
+
+    <?php require_once("parts/navbar.php"); ?>
+
+    <nav id="bj-subnavbar" class="navbar">
+        <div class="container-fluid" style="text-align: center" >
+            <div class="text-center">
+                <ul class="nav navbar-nav" style="margin: 0 auto;">
+                    <li><a href="#org-team" class="bj-smooth-scroll">Organizing Team</a></li>
+                    <li><a href="#contacts" class="bj-smooth-scroll">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header id="bj-page-header" class="bj-team">
+
+        <div id="bj-intro">
+
+            <h1> Team </h1>
+            <img id="bj-scrolldown-carrot" class="bj-smooth-scroll" src="img/chevron.png" />
+          
+        </div>
+
+    </header>
+
+    <section id="org-team" class="bj-box container bj-page-section">
+
+        <h1 class="bj-headline"> Organizing Team </h1>
+
+        <div class="row">
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Tana.jpg">
+
+                <h3 class="bjt-mem-name"> H.E. Ms. Tatiana Škultétyová </h3>
+
+                <h4 class="bjt-mem-funcname"> Deputy Secretary-General </h4>
+
+            </div>
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/KuboK.jpg">
+
+                <h3 class="bjt-mem-name"> H.E. Mr. Jakub Kučerák </h3>
+
+                <h4 class="bjt-mem-funcname"> Secretary-General </h4>
+
+		        <!--
+                <div class="row bj-contacts">
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-earphone bj-contact-phone" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-envelope bj-contact-email" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                </div>
+                -->
+
+            </div>
+	
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Kiko.jpg">
+
+                <h3 class="bjt-mem-name"> H.E. Mr. Kristián Rigasz </h3>
+
+                <h4 class="bjt-mem-funcname"> Deputy Secretary-General </h4>
+
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Dominik.jpg">
+
+                <h3 class="bjt-mem-name"> H.E. Mr. Dominik Hudec </h3>
+
+                <h4 class="bjt-mem-funcname"> Deputy Secretary-General </h4>
+
+            </div>
+
+          
+       <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Palo.jpg">
+
+                <h3 class="bjt-mem-name"> Mr. Pavol Margitfalvi </h3>
+
+                <h4 class="bjt-mem-funcname"> Head of IT Support </h4>
+
+            </div> 
+<div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Ema.jpg">
+
+                <h3 class="bjt-mem-name"> Ms. Ema Rýdza</h3>
+
+                <h4 class="bjt-mem-funcname"> Head of Delegation Services </h4>
+ </div>
+            
+		       
+        </div>
+    
+        <div class=>
+
+            <div class="col-sm-4 bjt-mem">
+
+                
+
+            </div><div class="col-sm-4 bjt-mem">
+
+
+            </div>  <div class="col-sm-4 bjt-mem">
+
+
+            </div>  
+
+
+     
+    <!--         <div class="row bj-contacts">
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-earphone bj-contact-phone" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-envelope bj-contact-email" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                </div>
+                
+		 
+           </div>
+		-->    
+       
+          
+ <!--
+        <div class="row">
+	<div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/Vicky.jpg">
+
+                <h3 class="bjt-mem-name"> Ms. Vicky Weissová </h3>
+
+                <h4 class="bjt-mem-funcname"> Sponsorship Manager and Head of PR </h4>
+
+            </div>
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/julia.png">
+
+                <h3 class="bjt-mem-name"> Ms. Júlia Horbaľová </h3>
+
+                <h4 class="bjt-mem-funcname">  </h4>
+
+		       
+                <div class="row bj-contacts">
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-earphone bj-contact-phone" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                    <div class="col-xs-6">
+
+                        <span class="glyphicon glyphicon-envelope bj-contact-email" data-container="body" data-toggle="popover" data-placement="bottom" data-content=""></span>
+
+                    </div>
+
+                </div>
+               
+
+            </div>
+
+            <div class="col-sm-4 bjt-mem">
+
+                <img class="img-circle img-responsive" src="img/team/karolina.png">
+
+                <h3 class="bjt-mem-name"> Ms. Janka Bednarčíková </h3>
+
+                <h4 class="bjt-mem-funcname"> Design and Creative </h4>
+
+            </div>
+
+        </div>
+ -->
+    </section>
+
+    <section id="contacts" class="container bj-page-section">
+
+        <h1 class="bj-headline"> Contacts </h1>
+
+        <div class="row col-sm-8 col-md-6 col-xl-4">
+
+            <div class="bjt-contact-group">
+
+          <!--      <h2> PhDr. Eva Žitná </h2>
+
+                <p> Director of the international school and senior organizing advisor </p>
+
+                <p> <a href="mailto: zitna&commat;gjh.sk"> zitna&commat;gjh.sk </a> </p>
+
+            </div> -->
+
+            <div class="bjt-contact-group">
+
+                <h2> RNDr. František Kosper </h2>
+
+                <p> Director of Novohradská Foundation and senior financial coordinator </p>
+
+                <p> <a href="mailto: kosper&commat;gjh.sk"> kosper&commat;gjh.sk </a> </p>
+
+            </div>
+
+            <div class="bjt-contact-group">
+
+                <h2> Jakub Kučerák </h2>
+
+                <p> Secretary-General </p>
+
+                <p> <a href="mailto: kubo.kucerak&commat;gmail.com"> kubo.kucerak&commat;gmail.com </a>, <a href="mailto: bratmun&commat;gmail.com"> bratmun&commat;bratmun.sk </a> </p>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <?php require_once("parts/footer.php"); ?>
+
+</div>
+
+</body>
+
+</html>
